311da208737ba2da732068330ce07917f3527327
AhmedAlDiab <ahmedaldiabdev@gmail.com>
2025-02-16 19:09:24 +0200

    AAD Sends Regards

---
+++ Source Code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guessing_random_number_game_With_CSharp_Basics
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAD Sends Regards");
+            Console.ReadKey();
+        }
+    }
+}
